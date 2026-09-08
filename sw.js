030e5d6cec87f27d25bf00c564f8e6e3a1509857
Avant-17 <jz2410100606@gmail.com>
2026-09-08 16:17:32 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cal-fin-v3';
+const CACHE = 'cal-fin-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
